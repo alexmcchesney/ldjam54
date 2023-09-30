d0e1a0382ee6d53ebf29ce1d34d1d4a43ac3c1f6
Alex McChesney <alex.mcchesney@gmail.com>
2023-09-30 15:51:18 -0500

    Added entrance/exits/room spawning

--- Assets/Scripts/Environment/Room.cs
+++ Assets/Scripts/Environment/Room.cs
@@ -1,5 +1,8 @@
+using Assets.Scripts.People;
+using Assets.Scripts.Utility;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Environment
@@ -7,6 +10,16 @@ namespace Assets.Scripts.Environment
     public class Room : MonoBehaviour
     {
         public GameObject Entrance;
+
+        public void OnExit()
+        {
+            // Clean up all people
+            GameObject[] livePeople = Person.LivePeople.ToArray();
+            foreach(GameObject person in livePeople) 
+            {
+                ObjectPool.PoolObject(person);
+            }
+        }
     }
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,61 @@
+using Assets.Scripts.Environment;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] RoomPrefabs;
+    [SerializeField]
+    private GameObject[] _roomPrefabs;
 
-    private int _currentRoom = 0;
+    [SerializeField]
+    private GameObject _player;
+
+    private int _currentRoomIndex = 0;
+
+    private GameObject _currentRoom;
+
+    public static GameManager Instance { get; private set; }
+
+    public void Awake()
+    {
+        Instance = this;
+    }
 
     public void OnEnable()
     {
         // There may be a room already loaded in the editor
+        var existing = FindObjectOfType<Room>();
+        if (existing == null)
+        {
+            SpawnRoom();
+        }
+        else
+        {
+            _currentRoom = existing.gameObject;
+        }
+    }
+
+    private void SpawnRoom()
+    {
+        if(_currentRoomIndex > _roomPrefabs.Length-1)
+        {
+            _currentRoomIndex = 0;
+        }
+
+        _currentRoom = Instantiate(_roomPrefabs[_currentRoomIndex], transform);
+        _currentRoom.transform.SetParent(null, false);
+        _currentRoom.transform.localPosition = Vector3.zero;
+        Room room = _currentRoom.GetComponent<Room>();
+        _player.transform.position = room.Entrance.transform.position;
+    }
+
+    public void NextRoom()
+    {
+        _currentRoomIndex++;
+        _currentRoom.GetComponent<Room>().OnExit();
+        Destroy(_currentRoom);
+        SpawnRoom();
     }
 }
