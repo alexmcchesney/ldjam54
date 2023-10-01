33379aef04e40e11d41959dc29213f622ebf47c9
Erto Pena <ertopena@gmail.com>
2023-10-01 15:55:44 -0500

    Add Doom Face prefab.

---
+++ Assets/Scripts/HUD/DoomFaceOption.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HUD
+{
+    [System.Serializable]
+    public class DoomFaceOption
+    {
+        [SerializeField] public Sprite sprite;
+        [SerializeField] public float upperAnxietyBound;
+    }
+}
